0c788bea4344eef89a6929bbd095812658312005
Alexander López Sánchez <Alopezs@ucenfotec.ac.cr>
2025-07-28 18:29:11 -0600

    Initial project structure

---
+++ resourceallocator/src/main/java/com/dhl/resourceallocator/ResourceallocatorApplication.java
@@ -0,0 +1,13 @@
+package com.dhl.resourceallocator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceallocatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceallocatorApplication.class, args);
+	}
+
+}
---
+++ resourceallocator/src/test/java/com/dhl/resourceallocator/ResourceallocatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhl.resourceallocator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceallocatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
